c3d4090e21b2df72011d582f5bf061f81762770e
allen19-77 <allen.19@list.ru>
2022-05-07 20:40:56 +0500

    tratslate comment

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-const createServer = require('http').createServer // импортируем функцию для создания сервера
+const  readFileSync  = require('fs').readFileSync
 
-const server = createServer(handleRequest) // создаем сервер	
+const createServer = require('http').createServer // importing a function to create a server-импортируем функцию для создания сервера
+
+const server = createServer(handleRequest) // create server-создаем сервер	
 const port = 3000 // задаем порт
 
 
 
-server.listen(port, informAboutStart) // запускаем сервер, передаем порт и функцию оповещения о старте работы сервера 
+server.listen(port, informAboutStart) // starting the server, pass the  port of the notification and function about the start of the server-запускаем сервер, передаем порт и функцию оповещения о старте работы сервера 
 
-/*require('http').createServer((request, response) => response.end('Hello, world!')).listen(3000)*/
+/*require('http').createServer((request, response) => response.end('Hello, world!')).listen(3000)  //it's short version create server*/
 
 
 function informAboutStart() {
 	console.log('Server started at http://localhost:' + port )
-} // функция информирование о старте работы сервера
+} // the function informing about the start of the server-функция информирование о старте работы сервера
 
 function handleRequest(request, response) {
-	response.end('Hello, world!')
-} // функция запроса на обработку
+	let url = request.url
+	if (url == '/') url = '/index'
+	const file = readFileSync('.' + url)
+	response.end(file)
+} // processing request function-функция запроса на обработку
